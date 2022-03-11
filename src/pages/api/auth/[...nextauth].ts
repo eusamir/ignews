477dd40d2685c57082f54a308fba8c8d3ee7bb85
Samir <samir.andrade@ceuma.br>
2022-03-11 18:16:00 -0300

    add nextauth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,11 +2,15 @@ import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      authorization: {
+        params: {
+          scope: 'read:user',
+        },
+      },
     }),
     // ...add more providers here
   ],
